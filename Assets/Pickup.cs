9f3cffd9de82679f8012bd4fe87a7aacb60102bd
niloorh <niloo80200@gmail.com>
2025-10-14 21:17:14 +0330

    VR

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -25,6 +25,17 @@ public class Pickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (OVRInput.GetDown(OVRInput.Button.One)) // A button
+        //{
+        //    Ray ray = new Ray(playerCamera.position, playerCamera.forward);
+        //    if (Physics.Raycast(ray, out RaycastHit hit, 3f))
+        //    {
+        //        if (hit.transform == transform)
+        //        {
+        //            gameManager.GetComponent<GameManager>().pickupObject(pickupType);
+        //            gameObject.SetActive(false);
+        //        }
+        //    }
+        //}
     }
 }
